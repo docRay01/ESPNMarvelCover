343ef83d594c43a55b2653ea58314d71505093cb
Davis, Steven <R.Steven.Davis@disney.com>
2022-04-28 20:44:14 -0400

    Basic project scaffold

---
+++ ESPNMarvelCover/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ESPNMarvelCover
+//
+//  Created by Davis, R. Steven on 4/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ESPNMarvelCover/ESPNMarvelCoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  ESPNMarvelCoverApp.swift
+//  ESPNMarvelCover
+//
+//  Created by Davis, R. Steven on 4/28/22.
+//
+
+import SwiftUI
+
+@main
+struct ESPNMarvelCoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
