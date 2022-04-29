a05307e83a198eadd79662dc855548ee71ab4df0
Davis, Steven <R.Steven.Davis@disney.com>
2022-04-29 16:17:42 -0400

    Add the two main views

--- ESPNMarvelCover/ESPNMarvelCoverApp.swift
+++ ESPNMarvelCover/ESPNMarvelCoverApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ESPNMarvelCoverApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ESPNMarvelHomeView()
         }
     }
 }
