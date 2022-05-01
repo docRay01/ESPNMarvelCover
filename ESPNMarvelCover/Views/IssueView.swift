795fe3d689885d2327f7f64460ed458f7dde6450
Davis, Steven <R.Steven.Davis@disney.com>
2022-05-01 14:42:06 -0400

    Revised issue layout

--- ESPNMarvelCover/Views/IssueView.swift
+++ ESPNMarvelCover/Views/IssueView.swift
@@ -13,18 +13,19 @@ struct IssueView: View {
     var body: some View {
         VStack {
             Text(issue?.name ?? "")
-            GeometryReader { metrics in
                 Image(uiImage: UIImage())
                     .resizable()
-                    .frame(width: metrics.size.width * 0.75, height: metrics.size.width * 0.75 * 1024 / 633, alignment: .center)
                     .aspectRatio(CGSize(width: 633, height: 1024), contentMode: .fit)
                     .background(Color.gray)
-                    .position(x: metrics.size.width/2, y: metrics.size.height/2)
-                    
-            }
-                Text(issue?.description ?? "")
-                Text("Issue display view")
-        }.background(Color.red)
+                    .layoutPriority(1)
+            .padding(EdgeInsets(top: 10,
+                                 leading: 80,
+                                 bottom: 10,
+                                 trailing: 80))
+            
+            Text(issue?.description ?? "")
+            Spacer()
+        }
     }
     
 }
@@ -33,7 +34,7 @@ struct IssueView_Previews: PreviewProvider {
     static var previews: some View {
         let issue = IssueModel(id: "12345",
                                name: "TestIsssue",
-                               imageURLString: "https://source.unsplash.com/user/c_v_r/100x100", description: "This is a test string. Lorem ipsum! Excelsior!")
+                               imageURLString: "https://source.unsplash.com/user/c_v_r/100x100", description: "This is a test string. \n \nLorem ipsum! Excelsior!")
         
         IssueView(issue: issue)
     }
